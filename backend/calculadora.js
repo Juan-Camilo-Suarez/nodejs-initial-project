be1d943405c76ce60372e8c13b6eaf3cd6fcc359
Juan-Camilo-Suarez <juancamilosuarez3@gmail.com>
2022-08-10 22:39:02 +0300

    codigo compilado en consola

--- backend/calculadora.js
+++ backend/calculadora.js
@@ -1,1 +1,20 @@
- 
+'use strict';
+
+//para leer los parametros de consola 
+var params = process.argv.slice(2);
+
+var numero1 =parseFloat(params[0])
+var numero2 =parseFloat(params[1])
+
+var plantilla = `
+    la  suma es: ${numero1 + numero2}
+    la  resta es: ${numero1 - numero2}
+    la  multiplicacion es: ${numero1 * numero2}
+    la  division es: ${numero1 / numero2}
+                `;
+
+console.log(plantilla);
+
+console.log(
+    "hola mundo con node"
+);
